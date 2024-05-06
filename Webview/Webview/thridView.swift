09067f0243b7bd04e5dfe03d09a8ad7d3150d984
Parthasai <53685926+propardhu@users.noreply.github.com>
2024-05-05 21:59:05 -0500

    name of viewing image

--- Webview/Webview/thridView.swift
+++ Webview/Webview/thridView.swift
@@ -15,21 +15,27 @@ struct thridView: View {
     @State private var takeScreenshot = false
     @State private var rootIP = "http://129.107.206.21:5001/"
     
-    func getURL() -> URL {
+    func getURL(path:String) -> URL {
         //        print(resultViweURL)
         print(rootIP)
-        let url = URL(string: (rootIP))
+        print(path.replacingOccurrences(of: "/home/data/nejm_ai/DATABASE/", with: ""))
+        let url = URL(string: (rootIP + path.replacingOccurrences(of: "/home/data/nejm_ai/DATABASE/", with: "")))
         return url!
     }
     
     var body: some View {
         VStack {
-//                        WebView(url: getURL(), takeScreenshot: $takeScreenshot)
-//            Text(noteId!.uuidString)
+            //                        WebView(url: getURL(), takeScreenshot: $takeScreenshot)
+            //            Text(noteId!.uuidString)
             if let index = dataStore.notes.firstIndex(
                 where: { $0.id == noteId }
             ) {
-                Text(dataStore.notes[index].name)
+                //                Text(dataStore.notes[index].name)
+                WebView(url: getURL(path: dataStore.notes[index].name), takeScreenshot: $takeScreenshot).ornament(visibility: .visible, attachmentAnchor: .scene(.bottom), contentAlignment: .center) {
+                    HStack {
+                        Text(dataStore.notes[index].name)
+                    }.glassBackgroundEffect()
+                }
             } else {
                 // If we ended up here, it means that the note detail window was state restored by SwiftUI, but we didn't implement data persistence, so we can't show the note.
                 Text("Couldn't find the presented note, because data persistence is not implemented in this sample project")
